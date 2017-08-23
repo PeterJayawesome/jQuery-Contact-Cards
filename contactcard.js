30e206828c0d9107290a7ed33bafe4ce42cd9238
Peng Jiang <pengj@usc.edu>
2017-08-22 18:03:18 -0700

    first commit

---
+++ contactcard.js
@@ -0,0 +1,37 @@
+function cardStyle(){
+  $('.card').css("text-align","center");
+  $('.card').css("margin","10px");
+  $('.card').css("width","300px");
+  $('.card').css("height","200px");
+  $('.card').css("border","1px solid black");
+  $('.name').css("display","inline-block");
+  $('.name').css("margin","10px");
+  $('.name').css("font-size","24px");
+  $('.c').css("position","relative");
+  $('.c').css("top","30px");
+  $('.descrip').css("display","none");
+
+}
+
+cardStyle();
+
+$(document).on('click','.card',function(){
+  $(this).children().toggle(500);
+})
+
+$('button').click(function(){
+  if($('#first').val() !=''&& $('#last').val() !=''&& $('#description').val() !=''){
+    // var acard="<div class='card'><p class='name'>"+$('#first').val()+"</p><p class='name'>"+$('#last').val()+"</p><p>Click for description!</p><p class='descrip'>"+$('#description').val()+"</p></div>";
+    // document.getElementById(card_area).innerHTML += acard;
+
+    $('#card_area').append("<div class = 'card'><div class='front'></div></div>");
+    $('#card_area .card:last-child .front').append("<p class='name'>"+$('#first').val()+'</p>');
+    $('#card_area .card:last-child .front').append("<p class='name'>"+$('#last').val()+'</p>');
+    $('#card_area .card:last-child .front').append("<p class='c'>Click for description!</p>");
+    $('#card_area .card:last-child').append("<p class='descrip'>"+$('#description').val()+"</p>");
+    cardStyle();
+    $('#first').val("");
+    $('#last').val("");
+    $('#description').val("");
+  }
+});
